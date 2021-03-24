df02d14f5811932c6d19ba8203efe7ac4240b91a
damage <jphandll@gmail.com>
2021-03-25 01:36:37 +0300

    damage
    add lab2

---
+++ script/getWeather.js
@@ -0,0 +1,47 @@
+async function getWeatherByCoord(lat, lon) {
+    const  response = await fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric&appid=20940a1010f61657e990376f1c7271b4", {
+        "method": "GET",
+    })
+    try {
+        const json = await response.json();
+        var weatherInfo = {
+            cityName: json.name,
+            weather: json.weather[0].main,
+            temp: json.main.temp,
+            wind: json.wind.speed + " m/s ," + json.wind.deg + " deg",
+            clouds: json.clouds.all+" %",
+            pressure: json.main.pressure+" 	hPa",
+            humidity: json.main.humidity+" %",
+            coord: "lon:" + json.coord.lon + " ,lat:" + json.coord.lat,
+            code:json.cod
+        };
+        return weatherInfo;
+    } catch (err) {
+        console.log(err);
+    };
+}
+
+async function getWeatherByCityName(name) {
+    const response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + name + "&units=metric&appid=20940a1010f61657e990376f1c7271b4", {
+        "method": "GET",
+    })
+    try {
+        const json = await response.json();
+        var weatherInfo = {
+            cityName: json.name,
+            weather: json.weather[0].main,
+            temp: json.main.temp,
+            wind: json.wind.speed + " m/s ," + json.wind.deg + " deg",
+            clouds: json.clouds.all+" %",
+            pressure: json.main.pressure+" 	hPa",
+            humidity: json.main.humidity+" %",
+            coord: "lon:" + json.coord.lon + " ,lat:" + json.coord.lat,
+            code:json.cod
+        };
+        return weatherInfo;
+    } catch (err) {
+        console.log(err);
+    };
+}
+
+
---
+++ script/lab2.js
@@ -0,0 +1,294 @@
+var iconDic={"Clear":`<i class="fas fa-sun"></i>`,"Rain":`<i class="fas fa-cloud-rain"></i>`,"Clouds":`<i class="fas fa-cloud"></i>`,"Snow":`<i class="fas fa-snowflakes"></i>`}
+var countCities = 0
+var cities = new Array();
+if (localStorage.length == 0) {
+    localStorage.cities = cities;
+}
+cities = localStorage.cities.split(",");
+
+var container = document.getElementsByClassName("container")[0];
+
+function deleteBlock(btn) {
+    var elem = btn.parentNode.parentNode.parentNode;
+    var block = btn.parentNode.parentNode;
+    var cityName = btn.parentNode.children[0].innerText;
+    console.log(cityName)
+    for (let i = 0; i < cities.length; i++) {
+        if (cities[i] == cityName) {
+            cities.splice(i);
+        }
+    }
+    localStorage.cities = cities;
+    console.log(localStorage.cities)
+    elem.removeChild(block);
+}
+
+function addLoadingBlock(cityName) {
+    cities.push(cityName)
+    localStorage.cities = cities;
+    container.innerHTML += ` <div class="section" >
+    <div class="city cities">
+        <div class="cityName block-inline">${cityName}
+        </div>
+        <div class="temp block-inline">
+                    ???
+                </div>
+                <div class="icon block-inline">
+                <i class="fas fa-question-circle"></i>
+                </div>
+        <div class="deleteBlock block-inline" onclick="deleteBlock(this)">
+            <i class="far fa-times-circle"></i>
+        </div>
+    </div>
+    <div class="weather loading">
+        <div style="margin-top:40px;margin-bottom:30px;">LOADING</div>
+        <i class="fas fa-redo-alt"></i>
+    </div>
+</div>`
+setCitiesWeather(cityName);
+
+}
+
+
+//加载存在LocalStrorage里的信息,
+function loadLocalStorageBlock() {
+    for (var i = 0; i < cities.length; i++) {
+        if (cities[i] != "") {
+            container.innerHTML += ` <div class="section" >
+            <div class="city cities">
+        <div class="cityName block-inline">${cities[i]} 
+        </div>
+        <div class="temp block-inline">
+                    ???
+                </div>
+                <div class="icon block-inline">
+                <i class="fas fa-question-circle"></i>
+                </div>
+        <div class="deleteBlock block-inline" onclick="deleteBlock(this)">
+            <i class="far fa-times-circle"></i>
+        </div>
+    </div>
+    <div class="weather loading">
+        <div style="margin-top:40px;margin-bottom:30px;">LOADING</div>
+        <i class="fas fa-redo-alt"></i>
+    </div>
+</div>`
+        }
+    }
+}
+
+var deleteBlockBtn = document.getElementsByClassName("deleteBlock");
+
+for (let i = 0; i < deleteBlockBtn.length; i++) {
+    deleteBlockBtn[i].setAttribute("onclick", "deleteBlock(this)");
+}
+
+
+var addBlockBtn = document.getElementsByClassName("fa-plus-circle")[0];
+var addBlockInput = document.getElementById("addBlockInput");
+addBlockBtn.setAttribute("onclick", `addLoadingBlock(document.getElementById("addBlockInput").value)`)
+
+
+
+
+
+const sleep = (timeout = 2000) => new Promise(resolve => {
+    setTimeout(resolve, timeout);
+});
+
+function getWeatherIcon(weather) {
+    return iconDic[weather];
+}
+
+const setMainCityWeather = async () => {
+    mainCityBlock = document.getElementsByClassName("header")[0];
+    loadingBlock = document.getElementsByClassName("loadingMainCity")[0];
+    loadingBlock.style.display = "block";
+    mainCityBlock.style.display = "none";
+
+    await sleep(2000);
+    var weatherInfo;
+    try {
+        weatherInfo = await getWeatherByCoord(localLat, localLon);
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+    console.log(weatherInfo.code);
+    var mainCityBlock = document.getElementsByClassName("header")[0];
+    if (weatherInfo.code == 200) {
+        var header = document.createElement("header");
+        header.innerHTML = `<div class="city mainCity">
+        <div class="cityName">${weatherInfo.cityName}
+        </div>
+        <div class="icon">
+            ${getWeatherIcon(weatherInfo.weather)}
+        </div>
+        <div class="temp">
+            ${weatherInfo.temp} °C
+        </div>
+    </div>
+
+    <div class="weather">
+        <ul>
+            <li class="wind weatherInfo">
+                <div class="block-inline weatherInfoName">
+                    <p>Ветер</p>
+                </div>
+                <div class="block-inline weatherInfoValue">
+                    <p>${weatherInfo.wind}</p>
+                </div>
+            </li>
+            <li class="cloud weatherInfo">
+                <div class="block-inline weatherInfoName">
+                    <p>облачность</p>
+                </div>
+                <div class="block-inline weatherInfoValue">
+                    <p>${weatherInfo.clouds}</p>
+                </div>
+            </li>
+            <li class="pressure weatherInfo">
+                <div class="block-inline weatherInfoName">
+                    <p>Давление</p>
+                </div>
+                <div class="block-inline weatherInfoValue">
+                    <p>${weatherInfo.pressure}</p>
+                </div>
+            </li>
+            <li class="humidity weatherInfo">
+                <div class="block-inline weatherInfoName">
+                    <p>Влажность</p>
+                </div>
+                <div class="block-inline weatherInfoValue">
+                    <p>${weatherInfo.humidity}</p>
+                </div>
+            </li>
+            <li class="coordinates weatherInfo">
+                <div class="block-inline weatherInfoName">
+                    <p>Координаты</p>
+                </div>
+                <div class="block-inline weatherInfoValue">
+                    <p>${weatherInfo.coord}</p>
+                </div>
+            </li>
+        </ul>
+    </div>`;
+        header.setAttribute("class", "header");
+        mainCityBlock.replaceWith(header);
+        loadingBlock.style.display = "none";
+    } else {
+        console.log("weather API error")
+    }
+}
+
+async function setCitiesWeather(cityName) {
+    var sectionBlocks = document.getElementsByClassName("section");
+    for (let i = 0; i < sectionBlocks.length; i++) {
+        if (sectionBlocks[i].children[0].children[0].innerText == cityName) {
+            (()=>{var date = new Date();console.log(date.toLocaleTimeString())
+            })();
+            await sleep(2000);
+            var weatherInfo;
+            try {
+                weatherInfo = await getWeatherByCityName(cityName);
+            } catch (error) {
+                console.log(error);
+                return;
+            }
+            if (weatherInfo.code == 200) {
+                var section = document.createElement("div");
+                section.setAttribute("class", "section");
+                section.innerHTML = `<div class="city cities">
+                <div class="cityName block-inline">
+                ${weatherInfo.cityName}
+                </div>
+                <div class="temp block-inline">
+                ${weatherInfo.temp}°C
+                </div>
+                <div class="icon block-inline">
+                ${getWeatherIcon(weatherInfo.weather)}
+                </div>
+                <div class="deleteBlock block-inline" onclick="deleteBlock(this)">
+                    <i class="far fa-times-circle"></i>
+                </div>
+            </div>
+            <div class="weather ">
+                <ul>
+                    <li class="wind weatherInfo">
+                        <div class="block-inline weatherInfoName">
+                            <p>Ветер</p>
+                        </div>
+                        <div class="block-inline weatherInfoValue">
+                            <p>${weatherInfo.wind}</p>
+                        </div>
+                    </li>
+                    <li class="cloud weatherInfo">
+                        <div class="block-inline weatherInfoName">
+                            <p>облачность</p>
+                        </div>
+                        <div class="block-inline weatherInfoValue">
+                            <p>${weatherInfo.clouds}</p>
+                        </div>
+                    </li>
+                    <li class="pressure weatherInfo">
+                        <div class="block-inline weatherInfoName">
+                            <p>Давление</p>
+                        </div>
+                        <div class="block-inline weatherInfoValue">
+                            <p>${weatherInfo.pressure}</p>
+                        </div>
+                    </li>
+                    <li class="humidity weatherInfo">
+                        <div class="block-inline weatherInfoName">
+                            <p>Влажность</p>
+                        </div>
+                        <div class="block-inline weatherInfoValue">
+                            <p>${weatherInfo.humidity}</p>
+                        </div>
+                    </li>
+                    <li class="coordinates weatherInfo">
+                        <div class="block-inline weatherInfoName">
+                            <p>Координаты</p>
+                        </div>
+                        <div class="block-inline weatherInfoValue">
+                            <p>${weatherInfo.coord}</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>`;
+                sectionBlocks[i].replaceWith(section)
+            } else {
+                console.log("weather API error")
+            }
+        }
+    }
+}
+
+const updateCitiesWeather= async () =>{
+    console.time("get cities weather")
+    const promises = cities.map(cityName => setCitiesWeather(cityName));
+    for(const promise of promises){
+         await promise;
+    }
+    console.timeEnd("get cities weather")
+}
+
+
+function updateWeather(){
+    (async () =>{
+        await setMainCityWeather();
+    })();
+    var sections = document.getElementsByClassName("section");
+    let t = sections.length;
+    for(let i=0;i<t;i++){
+        sections[0].parentNode.removeChild(sections[0]);
+    }
+    loadLocalStorageBlock();
+    updateCitiesWeather();
+}
+
+
+var updateWeatherBtns = document.getElementsByClassName("updateWeather");
+for(btn of updateWeatherBtns){
+    btn.setAttribute("onclick","updateWeather();")
+}
